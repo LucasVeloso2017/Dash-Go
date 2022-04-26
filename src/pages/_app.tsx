eb56e2e11c48668a076dc71f52a5946c857c9435
LucasVelloso2017 <lucas.velloso2017@gmail.com>
2022-04-26 18:28:34 -0300

    feat: create sidebar context, add provider to index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { SidebarDrawerProvider } from "../context/SidebarDrawerContext";
+import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ColorModeProvider options={theme}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </ColorModeProvider>
+  );
 }
 
 export default MyApp;
