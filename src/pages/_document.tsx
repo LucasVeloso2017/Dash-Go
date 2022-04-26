25c5beac1eb919666be01b188faa0f7cac88a774
LucasVelloso2017 <lucas.velloso2017@gmail.com>
2022-04-26 18:27:37 -0300

    feat: add next document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import { ColorModeScript } from "@chakra-ui/react";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <ColorModeScript initialColorMode="dark" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
